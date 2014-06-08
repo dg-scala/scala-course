71864c34974bb02557242fceb42b66b2c1d5673a
Dragan Glumac <dragan.glumac@gmail.com>
2014-06-08 18:22:14 +0100

    First submission.

--- Scala/Assignments/week7/streams/src/main/scala/streams/StringParserTerrain.scala
+++ Scala/Assignments/week7/streams/src/main/scala/streams/StringParserTerrain.scala
@@ -52,7 +52,17 @@ trait StringParserTerrain extends GameDef {
    * a valid position (not a '-' character) inside the terrain described
    * by `levelVector`.
    */
-  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = ???
+  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = {
+    def isTerrain(c: Char): Boolean = (c == 'o' || c == 'S' || c == 'T')
+    pos =>
+      if (pos.x < 0 || pos.x >= levelVector.length) false
+      else {
+        lazy val rowVector = levelVector apply pos.x
+   
+        if (pos.y < 0 || pos.y >= rowVector.length) false
+        else isTerrain(rowVector apply pos.y)
+      }
+  }
 
   /**
    * This function should return the position of character `c` in the
@@ -62,7 +72,11 @@ trait StringParserTerrain extends GameDef {
    * Hint: you can use the functions `indexWhere` and / or `indexOf` of the
    * `Vector` class
    */
-  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos = ???
+  def findChar(c: Char, levelVector: Vector[Vector[Char]]): Pos =
+	(for {
+      v <- levelVector
+      if (v.indexOf(c) != -1)
+    } yield new Pos(levelVector.indexOf(v), v.indexOf(c))) apply 0
 
   private lazy val vector: Vector[Vector[Char]] =
     Vector(level.split("\n").map(str => Vector(str: _*)): _*)
